b0c4e7c2f7b1b88be0223ab6c96cc4083942e681
PedroLucenaAlves <alvespedroalonso@gmail.com>
2026-05-14 22:52:35 -0300

    chore: initial commit do spring initializr

---
+++ src/main/java/com/br/redec/accessos_terceros_ms/AccessosTercerosMsApplication.java
@@ -0,0 +1,13 @@
+package com.br.redec.accessos_terceros_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessosTercerosMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessosTercerosMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/redec/accessos_terceros_ms/AccessosTercerosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.redec.accessos_terceros_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessosTercerosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
